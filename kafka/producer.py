54cc7779a7672823321bbe4c711b07c5057751ea
Jaime Nufio <jaimeenufio@gmail.com>
2022-01-31 16:51:19 -0500

    _

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,7 +2,7 @@ from kafka import KafkaProducer
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 for num in range(100):
-    producer.send('a', b'ahh')
+    producer.send('a', str.encode(str(num)))
     print(num)
 
 print("produced")
