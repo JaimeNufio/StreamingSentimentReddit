84a080ea2273fcfd8b41dfe09ba76a263ddf2a93
Jaime Nufio <jaimeenufio@gmail.com>
2022-02-03 14:58:51 -0500

    testing getting all comments from a user

--- redditapi/reddit.py
+++ redditapi/reddit.py
@@ -14,6 +14,34 @@ reddit = praw.Reddit(
     user_agent=auth['user_agent']
 )
 
-for submission in reddit.subreddit("learnpython").hot(limit=10):
-    print(submission.title)
-    pprint.pprint(vars(submission).keys())
+def subreddit():
+    for submission in reddit.subreddit("learnpython").hot(limit=10):
+        print(submission.title)
+        pprint.pprint(vars(submission).keys())
+
+def user():
+    #look intogenerator
+    comments = reddit.redditor("thinkingwithportal").comments.top('all',limit=None);
+
+    cnt = 0;
+    for x in comments:
+        cnt+=1
+        print(x.body[:144],str(cnt))
+
+
+user()
+
+"""
+Goals
+
+Basically, we aim to make a spider. 
+We want to be able to target a single subreddit. 
+We want to get top posts.
+We want to get all comments from that page.
+We want to be able to keep track of posts as they are update? ## Is pgsql a good choice?
+-> Crawl via subreddits related to user?
+
+We want to find users
+we want to be able to get all posts.
+We want to be able to keep track.
+"""
